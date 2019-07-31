ddb5d109949aaddff2e72a9a2b5ef312011c525d
SANDEEP SHIVEN <39735595+sandeepshiven@users.noreply.github.com>
2019-07-31 17:19:27 +0530

    Update main.c

--- Sort the Array in an Ascending/main.c
+++ Sort the Array in an Ascending/main.c
@@ -1,1 +1,37 @@
+#include"stdio.h"
 
+void sort(int *, int);
+
+
+
+void main()
+  {
+      int i,size;
+      printf("Enter the size of array\n");
+      scanf("%d",&size);
+      printf("Enter the elements of the array\n");
+      int arr[size];
+      for(i =0;i<size;i++)
+      {
+           scanf("%d",&arr[i]);
+       }
+      sort(arr,size);
+  }
+
+
+void sort(int *ptr, int size)
+  {
+      int i,j,temp;
+   
+      for(i = 0;i < size; i++)
+        {
+            for(j=i+1;j<size;j++)
+              {
+                  if(*(ptr+j)<*(ptr+i))
+                    {
+                        temp = *(ptr +j);
+                        (
+
+      
+
+  
