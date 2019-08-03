b242958ecd2ff87e2a9da25f43f0a7b3d7d3e250
sandeepshiven <sandeepshiven0@gmail.com>
2019-08-04 02:23:12 +0530

    ques 19

--- Implement two Stacks using a Single Array & Check for Overflow & Underflow/main.c
+++ Implement two Stacks using a Single Array & Check for Overflow & Underflow/main.c
@@ -2,6 +2,17 @@
 
 int top_index_a=-1;
 int top_index_b;
+int pop(int *,int ,int );
+void stack_a(int *,int );
+void stack_b(int *,int );
+int push(int *,int ,int );
+int pop(int *,int ,int );
+
+
+
+
+
+
 
 
 void main()
@@ -10,7 +21,7 @@ void main()
     printf("Enter the size of the array in which stack A and B are being stored\n");
     scanf("%d",&max);
     int arr[max];
-    top_index_b = max-1;
+    top_index_b = max;
     while(1)
     {
 
@@ -29,64 +40,64 @@ void main()
 
 
 
-void stack_a(int arr[],max)
-{
-    int choice_operation = 0,item,stack = 0;
-    while(choice_operation != 3)
-    {
-        printf("What operation do you want to perform on stack A?\n1.insertion\n2.deletion\n3.end");
-        scanf("%d",&choice_operation);
-        switch(choice_operation)
-        {
-            case 1:
-                {
-
-                    item = push(arr,max,top_index_a,stack);
-                    printf("The top item of stack A is %d\n",item);
-                    break;
-                }
-
-            case 2:
-                {
-                    item = pop(arr,max,top_index_a,stack);
-                    printf("The top item of stack A is %d\n",item);
-                    break;
-                }
-
-            case 3:
-                {
-                    break;
-                }
-
-            default : printf("Invalid selection\n");
-        }
-    }
-
-
-
-}
-
-
-void stack_b(int arr[],max)
+void stack_a(int arr[],int max)
 {
     int choice_operation = 0,item,stack = 1;
     while(choice_operation != 3)
     {
-        printf("What operation do you want to perform on stack B?\n1.insertion\n2.deletion\n3.end");
+        printf("What operation do you want to perform on stack A?\n1.insertion\n2.deletion\n3.end\n");
         scanf("%d",&choice_operation);
         switch(choice_operation)
         {
             case 1:
                 {
 
-                    item = push(arr,max,top_index_b,stack);
+                    item = push(arr,max,stack);
+                    printf("The top item of stack A is %d\n",item);
+                    break;
+                }
+
+            case 2:
+                {
+                    item = pop(arr,max,stack);
+                    printf("The top item of stack A is %d\n",item);
+                    break;
+                }
+
+            case 3:
+                {
+                    break;
+                }
+
+            default : printf("Invalid selection\n");
+        }
+    }
+
+
+
+}
+
+
+void stack_b(int arr[],int max)
+{
+    int choice_operation = 0,item,stack = 2;
+    while(choice_operation != 3)
+    {
+        printf("What operation do you want to perform on stack B?\n1.insertion\n2.deletion\n3.end\n");
+        scanf("%d",&choice_operation);
+        switch(choice_operation)
+        {
+            case 1:
+                {
+
+                    item = push(arr,max,stack);
                     printf("The top item of stack B is %d\n",item);
                     break;
                 }
 
             case 2:
                 {
-                    item = pop(arr,max,top_index_b,stack);
+                    item = pop(arr,max,stack);
                     printf("The top item of stack B is %d\n",item);
                     break;
                 }
@@ -106,12 +117,12 @@ void stack_b(int arr[],max)
 
 
 
-int push(int arr,int max,int index,int stack)
+int push(int arr[],int max,int stack)
 {
     int item;
-    if (stack)
+    if (stack == 1)
     {
-        if(top_index_a + top_index_b>max)
+        if(top_index_a + (max-top_index_b)>=max-1)
         {
             printf("Stack Overflow\n");
             return -1;
@@ -127,7 +138,7 @@ int push(int arr,int max,int index,int stack)
     }
     else
     {
-        if(top_index_a + top_index_b=>max)
+        if(top_index_a + (max-top_index_b)>=max-1)
         {
             printf("Stack Overflow\n");
             return -1;
@@ -136,9 +147,55 @@ int push(int arr,int max,int index,int stack)
         {
             printf("Enter the item you want to insert\n");
             scanf("%d",&item);
-            top_index_;
-            arr[top_index_a] = item;
+            top_index_b--;
+            arr[top_index_b] = item;
             return item;
         }
     }
 }
+
+
+int pop(int arr[],int max,int stack)
+{
+    int item;
+    if (stack == 1)
+    {
+        if (top_index_a < 0)
+        {
+            printf("\n Stack Underflow\n");
+            return -1;
+        }
+
+        else
+        {
+            item = arr[top_index_a];
+            top_index_a--;
+            return item;
+        }
+
+    }
+
+    else
+    {
+        if (top_index_b >= max)
+        {
+            printf("\n Stack Underflow\n");
+            return -1;
+        }
+
+        else
+        {
+            item = arr[top_index_b];
+            top_index_b++;
+            return item;
+        }
+
+    }
+}
+
+
+
+
+
+
+
